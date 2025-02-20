4032f0cc3ea1b53b65faa33052d6d4e7bd1afdfc
lifuxiao <1816301009@st.gxu.edu.cn>
2025-02-20 09:57:42 +0800

    Update and rename 分类模型的Dataloader.py to Dataloader.py

--- code/ML/Dataloader.py
+++ code/ML/Dataloader.py
@@ -13,9 +13,9 @@ class CustomDataset(Dataset):
     def __init__(self, csv_file, root_dir, transform=None):
         """
         Args:
-            csv_file (string): CSV文件的路径。
-            root_dir (string): 包含所有图像的目录。
-            transform (callable, optional): 可选的transform来应用于样本。
+            csv_file (string): files
+            root_dir (string): Directory where the image is located
+            transform (callable, optional): 
         """
         self.data_frame = pd.read_csv(csv_file)
         self.root_dir = root_dir
@@ -30,17 +30,17 @@ class CustomDataset(Dataset):
 
         img_name = os.path.join(self.root_dir, self.data_frame.iloc[idx, 0])
         image = Image.open(img_name)
-        image_class = int(self.data_frame.iloc[idx, -1])  # 假设'class'是最后一列
+        image_class = int(self.data_frame.iloc[idx, -1])  # Case where class is the last line
 
         if self.transform:
             image = self.transform(image)
 
         return image, image_class
 
-# 创建数据集和数据加载器
-dataset = CustomDataset(csv_file='E:/分类任务/224_224/sample_output.csv', root_dir='E:/分类任务/224_224', transform=transform)
+# Creating datasets and data loaders
+dataset = CustomDataset(csv_file='E:/sample_output.csv', root_dir='E:/', transform=transform)
 dataloader = DataLoader(dataset, batch_size=4, shuffle=True, num_workers=0)
 
-# 使用dataloader
+# use dataloader
 for images, labels in dataloader:
     print(images.shape, labels)
